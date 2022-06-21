8430b58e16ca687dd76ce9468d132b192505d4e9
Dawid Wroński <dawid.filip.wronski@gmail.com>
2022-06-21 09:46:34 +0200

    +-/* features add, ac function add

--- script.js
+++ script.js
@@ -3,110 +3,130 @@ let operationButtons = document.querySelectorAll('.operation');
 let numbers = document.querySelectorAll('.number');
 let operationsBox = document.querySelector('.operations');
 let resultBox = document.querySelector('.result');
-let operation = '';
-let currentNumber;
-let previousNumber;
-let result;
+let resultButton = document.querySelector('.result-btn');
+let resetButton = document.querySelector('.reset');
+
+let calc = {
+    firstValue: '' ,
+    secondValue: '',
+    inputNumber: '',
+    operation: '',
+    result: '',
+    allowToSecond: false,    
+};
 
 numbers.forEach(number => {
     number.addEventListener('click', (e) => {
-        selectValue = e.target.textContent;
-        if ( operation == '' && currentNumber != undefined ) {
-            console.log('wchodze');
-            currentNumber += selectValue;
+        if ( typeof calc.result == 'number' ) {
+            calc = {
+                firstValue: '' ,
+                secondValue: '',
+                inputNumber: '',
+                operation: '',
+                result: '',
+                allowToSecond: false,
+            }
+            calc.result = printResult(calc.result, resultBox);
         }
-        else if ( currentNumber != undefined && operation != '' ) {
-            previousNumber = currentNumber;
-            currentNumber = selectValue;
-        }                
-        else {
-            currentNumber = selectValue;
-        }
-        console.log(currentNumber);
-        console.log(previousNumber);
-        console.log(operation);
-        doMath(operation);
+        calc.inputNumber += e.target.textContent;
+        console.log( typeof +calc.inputNumber);
+        printResult(calc.inputNumber, operationsBox);
+                
     })
 });
 
-function doMath(whatToDo) {
-    switch (whatToDo) {
+operationButtons.forEach(opButton => {
+    opButton.addEventListener('click', (e) => {
+        if( calc.firstValue == '' ) {
+            calc.firstValue = calc.inputNumber;
+            calc.inputNumber = '';
+            allowToSecond = true;
+            calc.operation = e.target.textContent;
+        }
+        if ( typeof calc.result == 'number' ) {
+            console.log('jestem tutaj');
+            calc.firstValue = calc.result + '';
+            calc.result = '';
+            calc.operation = e.target.textContent;
+            calc.inputNumber = '';
+            allowToSecond = true;
+            console.log(calc.result);
+        } 
+    })
+});
+
+function doMath(a, b, operation) {
+    switch (operation) {
         case '+':
-            result = +previousNumber + +currentNumber;
-            resultBox.textContent = result;            
+            calc.result = +a + +b;
+            printResult(calc.result, resultBox);
             break;
         case '-':
-            result = +previousNumber - +currentNumber;
-            resultBox.textContent = result;
+            calc.result = +a - +b;
+            printResult(calc.result, resultBox);
+            break;  
+        case '*':
+            calc.result = +a * +b;
+            printResult(calc.result, resultBox);
             break;
-        case '*': 
-            result = +previousNumber * +currentNumber;
-            resultBox.textContent = result;
-            break;
-        case '/': 
-            result = +previousNumber / +currentNumber;
-            resultBox.textContent = result;
-            break;      
+        case '/':
+            calc.result = +a / +b;
+            printResult(calc.result, resultBox);
+            break;          
+    
         default:
             break;
     }
+}
+
+function printResult(whatToPrint, whereToPrint) {
+    whereToPrint.textContent = whatToPrint;
 }
 
 function setOperation() {
     operation = this.textContent;    
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// add opacity 0.5 to button when click and then remove it after 100ms
+function visualKeyboardEffect(e) { 
+    e.target.classList.toggle('clicked');
+    setTimeout(() => {
+     e.target.classList.toggle('clicked');
+    }, 100);
+ }
+ function ac() {
+    calc = {
+    firstValue: '' ,
+    secondValue: '',
+    inputNumber: '',
+    operation: '',
+    result: '',
+    allowToSecond: false,
+    }
+    printResult(calc.result,resultBox);
+    printResult(calc.inputNumber, operationsBox);
+ }
+resetButton.addEventListener('click', ac);
 operationButtons.forEach(operationButton => {
     operationButton.addEventListener('click', setOperation);
 });
+
+resultButton.addEventListener('click', () => {
+    if ( calc.firstValue.length > 0 && allowToSecond == true ) {
+        calc.secondValue = calc.inputNumber;
+        calc.inputNumber = '';
+        console.log(calc.firstValue);
+        console.log(calc.secondValue);
+        console.log(calc.operation);
+        doMath(calc.firstValue, calc.secondValue, calc.operation);
+    }
+})
 
 buttons.forEach(button => {
     button.addEventListener('click', visualKeyboardEffect);
 });
 
-// add opacity 0.5 to button when click and then remove it after 100ms
-function visualKeyboardEffect(e) { 
-   e.target.classList.toggle('clicked');
-   setTimeout(() => {
-    e.target.classList.toggle('clicked');
-   }, 100);
-}
+
 
 
 
